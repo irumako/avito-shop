a61c177d95db9c6e5b2bb3d095b6d48097ac537a
irumako <irumakoTS@gmail.com>
2025-02-15 18:24:54 +0300

    feat: connection to db

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"github.com/caarlos0/env/v11"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 	"os"
@@ -14,6 +17,21 @@ const version = "1.0.0"
 
 type config struct {
 	Port int `env:"PORT" envDefault:"8080"`
+	Db   struct {
+		User         string `env:"DATABASE_USER"`
+		Password     string `env:"DATABASE_PASSWORD"`
+		Host         string `env:"DATABASE_HOST"`
+		Port         string `env:"DATABASE_PORT"`
+		Name         string `env:"DATABASE_NAME"`
+		MaxOpenConns int    `env:"MAX_OPEN_CONNS"`
+		MaxIdleConns int    `env:"MAX_IDLE_CONNS"`
+		MaxIdleTime  string `env:"MAX_IDLE_TIME"`
+	}
+}
+
+func (cfg *config) getDSN() string {
+	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		cfg.Db.User, cfg.Db.Password, cfg.Db.Host, cfg.Db.Port, cfg.Db.Name)
 }
 
 type application struct {
@@ -35,6 +53,20 @@ func main() {
 
 	log.Printf("Конфигурация: %+v", cfg)
 
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+
+		}
+	}(db)
+
+	logger.Printf("Подключение к БД установлено")
+
 	app := &application{
 		config: cfg,
 		logger: logger,
@@ -49,6 +81,31 @@ func main() {
 	}
 
 	logger.Printf("Запуск сервера на %s", srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.getDSN())
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(cfg.Db.MaxOpenConns)
+	db.SetMaxIdleConns(cfg.Db.MaxIdleConns)
+
+	duration, err := time.ParseDuration(cfg.Db.MaxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+	db.SetConnMaxIdleTime(duration)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
