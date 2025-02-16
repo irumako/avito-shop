8023e004e3b4f9bb262dc96469aca1ee067b327f
irumako <irumakoTS@gmail.com>
2025-02-16 08:46:49 +0300

    fix: change handlers methods

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -15,8 +15,8 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodGet, "/api/info", app.requireAuthenticatedUser(app.getInfoHandler))
 	router.HandlerFunc(http.MethodPost, "/api/sendCoin", app.requireAuthenticatedUser(app.sendCoinHandler))
-	router.HandlerFunc(http.MethodPost, "/api/buy/:item", app.requireAuthenticatedUser(app.buyProductHandler))
-	router.HandlerFunc(http.MethodGet, "/api/auth", app.createAuthenticationTokenHandler)
+	router.HandlerFunc(http.MethodGet, "/api/buy/:item", app.requireAuthenticatedUser(app.buyProductHandler))
+	router.HandlerFunc(http.MethodPost, "/api/auth", app.createAuthenticationTokenHandler)
 
 	return app.recoverPanic(app.authenticate(router))
 }
